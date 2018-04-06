e89457a7b1b4ae70144d526e8a9471efeadc02b2
lim <lim>
2018-04-06 13:07:30 +0800

    golog

---
+++ default_logger.go
@@ -0,0 +1,95 @@
+package log
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"image/color"
+)
+
+const (
+	FATAL = iota
+	PANIC
+	ERROR
+	WARN
+	INFO
+	DEBUG
+)
+
+type defaultLogger struct {
+	*log.Logger
+	Level int
+}
+
+func (l *defaultLogger) output(level int, levelKey string, v ...interface{}) error {
+	if l.Level >= level {
+		h := header(levelKey, fmt.Sprintf(v...))
+		return l.output(calldepth, h)
+	}else {
+		return nil
+	}
+}
+
+func (l *defaultLogger) outputf(level int, levelKey string, format string, v ...interface{}) error {
+	if l.Level >= level {
+		h := header(levelKey, fmt.Sprintf(format, v...))
+		return l.output(calldepth, h)
+	} else {
+		return nil
+	}
+}
+
+
+func (l *defaultLogger) Debug(v ...interface{}) {
+	l.output(DEBUG, "DEBUG", v)
+}
+
+func (l *defaultLogger) Debugf(format string, v ...interface{}) {
+	l.outputf(DEBUG, "DEBUG", format, v...)
+}
+
+func (l *defaultLogger) Info(v ...interface{}) {
+	l.output(INFO, "INFO", v)
+}
+
+func (l *defaultLogger) Infof(format string, v ...interface{}) {
+	l.outputf(INFO, "INFO", format, v...)
+}
+
+func (l *defaultLogger) Warn(v ...interface{}) {
+	l.output(WARN, "WARN", v)
+}
+
+func (l *defaultLogger) Warnf(format string, v ...interface{}) {
+	l.outputf(WARN, "WARN", format, v...)
+}
+
+func (l *defaultLogger) Error(v ...interface{}) {
+	l.output(ERROR, "ERROR", v)
+}
+
+func (l *defaultLogger) Errorf(format string, v ...interface{}) {
+	l.outputf(ERROR, "ERROR", format, v...)
+}
+
+func (l *defaultLogger) Fatal(v ...interface{}) {
+	l.output(FATAL, "FATAL", v)
+	os.Exit(1)
+}
+
+func (l *defaultLogger) Fatalf(format string, v ...interface{}) {
+	l.outputf(FATAL, "FATAL", format, v...)
+	os.Exit(1)
+}
+
+func (l *defaultLogger) Panic(v ...interface{}) {
+	l.Logger.Panic(v)
+}
+
+func (l *defaultLogger) Panicf(format string, v ...interface{}) {
+	l.Logger.Panicf(format, v...)
+}
+
+func header(lvl, msg string) string {
+	return fmt.Sprintf("%s: %s", lvl, msg)
+}
